02e84b601c9bb92a413e4301ce4b1b6d33062245
PrettyStreett <pretty.street.003@gmail.com>
2022-01-30 11:39:55 +0600

    Added html task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
+const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+
+// plugins for html
+const webpHTML = require('gulp-webp-html');
+const version = require('gulp-version-number');
+const fileinclude = require('gulp-file-include');
+
+const plumber = require('gulp-plumber');
+const notify = require("gulp-notify");
+
+
+
+const buildFolder = 'dist';
+const sourceFolder = 'src';
 
 const path = {
   build: {
@@ -26,3 +42,56 @@ const path = {
   },
   clean: `./${buildFolder}/`,
 };
+
+function html() {
+  return src(path.src.html)
+    /*
+    .pipe(plumber(
+      {errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    */
+    .pipe(fileinclude())
+    .pipe(webpHTML())
+    .pipe(version(
+    {
+        'value' : '%DT%',
+        'append' : {
+            'key' : '_v',
+            'cover' : 0,
+            'to' : [
+                'css',
+                'js'
+            ]
+        },
+        'output' : {
+            'file' : `${buildFolder}/version.json`
+        }
+    }))
+    .pipe(dest(path.build.html))
+    .pipe(browserSync.stream());
+}
+
+function browserSyncInit() {
+  browserSync.init({
+    server: {
+      baseDir: `./${buildFolder}/`,
+    },
+    port: 3333,
+    notify: false,
+    open: false,
+  });
+}
+
+function watchFiles() {
+  gulp.watch([path.watch.html], html);
+}
+
+const build = gulp.series(gulp.parallel(html));
+const watch = gulp.parallel(browserSyncInit, watchFiles, build);
+
+exports.html = html;
+
+exports.build = build;
+exports.watch = watch;
+
+exports.default = watch;
